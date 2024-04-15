62446ad774a670dd94488bb184399b7450874cbd
Shilo <shiloworks@gmail.com>
2024-04-15 16:18:23 +1000

    Fixed RXJS error

--- typescript/api/services/OMEROService.ts
+++ typescript/api/services/OMEROService.ts
@@ -1,4 +1,4 @@
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs/Rx';
 import {Sails, Model} from 'sails';
 import * as requestPromise from "request-promise";
 import * as _ from 'lodash';
